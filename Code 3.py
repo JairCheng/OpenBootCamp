b208c6130c10706e679d5c397faa44f73bf7838d
Jair Cheng <45506508+JairCheng@users.noreply.github.com>
2022-12-03 21:42:16 -0500

    Add files via upload

---
+++ Code 3.py
@@ -0,0 +1,18 @@
+# variable that needs to be created: sale price x comission % divide by 100
+
+loop = "y"
+loop2 = "y"
+
+def ComissionCalc(price, percentage):
+    print(float(price) * float(percentage) / 100)
+
+while loop2 == "y":
+    data1 = input("Enter product price: ")
+    data2 = input("Enter comission percentage: ")
+    ComissionCalc(data1, data2)
+    loop = input("Calculate again? y/n: ")
+    loop2 = loop.lower()
+
+
+print("Have a nice day!")
+
